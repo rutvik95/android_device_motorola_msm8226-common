644b64d9b5e5c7b5f41fdb43b4ee6ab2bb6dd4a3
Rutvik Rajagopal <rutvik_95@yahoo.co.in>
2014-06-29 17:57:42 +0530

    removed un-needed files for grand 2

--- README.md
+++
@@ -1,4 +0,0 @@
-Common device configuration for Motorola msm8226 based devices
-==============================
-
-Copyright 2013 - The CyanogenMod Project
--- keylayout/keylayout.mk
+++
@@ -1,4 +0,0 @@
-LOCAL_PATH := device/motorola/msm8226-common
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-	$(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
